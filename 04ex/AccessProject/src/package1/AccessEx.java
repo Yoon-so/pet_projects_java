0065a7259e0f709f3565e210d1c99722cc33ec78
Ekaterina <yoonrin113@gmail.com>
2026-04-27 10:54:22 +0900

    Add: Access.java and Sample.java

---
+++ 04ex/AccessProject/src/package1/AccessEx.java
@@ -0,0 +1,12 @@
+package package1;
+
+import package2.Sample;
+
+public class AccessEx {
+    public static void main(String[] args) {
+        Sample aClass = new Sample();
+        aClass.a = 10;
+        aClass.b = 10;
+        aClass.c = 10;
+    }
+}
---
+++ 04ex/AccessProject/src/package2/Sample.java
@@ -0,0 +1,7 @@
+package package2;
+
+public class Sample {
+    public int a;
+    public int b;
+    public int c;
+}
